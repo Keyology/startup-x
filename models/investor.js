10e53cc1c9a2ab4ffeb6672a90560527f07ee207
Keoni Murray <keonimurray45@gmail.com>
2019-01-10 13:35:43 -0800

    added module export to model

--- models/investor.js
+++ models/investor.js
@@ -4,23 +4,23 @@ const Schema = mongoose.Schema;
 InvestorSchema = new Schema({
     account_id: Schema.Types.ObjectId,
 
-    name:{
+    name: {
         type: String,
         required: true
 
-    }, 
+    },
     occupation: String,
-    email:{
-        type:String,
+    email: {
+        type: String,
         required: true,
         unique: true
     },
-    password:{
-        type: String, 
-        required: true, 
+    password: {
+        type: String,
+        required: true,
 
-    }, 
-    companies_invested:[{
+    },
+    companies_invested: [{
         type: Schema.Types.ObjectId,
         ref: 'Startup'
     }],
@@ -28,18 +28,14 @@ InvestorSchema = new Schema({
         type: Date,
         default: Date.now
     },
-   
+
 
     bio: String,
     Age: Number,
+});
 
 
 
-    
 
 
-
-})
-
-
-
+module.exports = mongoose.model('Investor', InvestorSchema);
